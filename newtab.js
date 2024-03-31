2c22b17fe844bc5ff617e8ec552f0df782e82080
isupercoder <info@fahdmurtaza.com>
2024-04-01 04:49:25 +0500

    Add icons and make adjustments to teh styling

--- newtab.js
+++ newtab.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://api.unsplash.com/photos/random?client_id=YOUR_UNSPLASH_ACCESS_KEY&query=travel')
+    fetch('https://api.unsplash.com/photos/random?client_id=GKZDtbYEQvhLjgs5ttk4ioOcuK7Y5HZJGFNCBdVCapI&query=travel')
     .then(response => response.json())
     .then(data => {
         document.getElementById('photo-container').innerHTML = `<img src="${data.urls.regular}" alt="Travel Photo">`;
